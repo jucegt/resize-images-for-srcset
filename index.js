c28d4fee4161a971ed33a531c3efba8b8c0ca305
jucegt <julio2201@gmail.com>
2021-04-26 16:46:13 -0600

    Changed config and added generate a new format

--- index.js
+++ index.js
@@ -8,22 +8,36 @@ const sharp = require('sharp');
 const files = glob.sync(config.input);
 
 config.sizes.forEach(size => {
-	const dir = `${config.output}${size.name}`
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir, { recursive: true }, error => {
-			if (error) throw error;
-		})
-	}
+  const dir = `${config.output}${size.name}`
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true }, error => {
+      if (error) throw error;
+    })
+  }
 
-	files.forEach(file => {
-		const filename = path.basename(file);
-		const  image = sharp(file);
+  files.forEach(file => {
+    const filename = path.basename(file);
+    const  image = sharp(file);
+    const imageConfig = { width: size.width, height: size.height, fit: config.fit };
 
-		image
-			.resize({width: size.width, height: size.height, fit: config.fit})
-			.toFile(`${dir}/${filename}`)
-			.catch(error => {
-				console.log(error);
-			});
-	});
+    image
+      .resize(imageConfig)
+      .toFile(`${dir}/${filename}`)
+      .catch(error => {
+        console.log(error);
+      });
+
+    config.formats.forEach(format => {
+      const filesplit = filename.split('.');
+      const defaultFormat = filesplit[filesplit.length-1];
+
+      image
+        .toFormat(format)
+        .resize(imageConfig)
+        .toFile(`${dir}/${filename.replace(defaultFormat, format)}`)
+        .catch(error => {
+          console.log(error);
+        });
+    });
+  });
 });
